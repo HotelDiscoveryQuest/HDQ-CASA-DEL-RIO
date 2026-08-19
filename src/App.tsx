0e1d56d3b3a801a28fe27594bfacde720fefdb92
HotelDiscoveryQuest <ainurizzatikhairunnisa@gmail.com>
2026-08-19 21:42:45 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3194,47 +3194,6 @@ function finishCard() {
   </div>
 )}
 
-    <button
-      onClick={() => setShowScanner(true)}
-    >
-      📷 Scan Card QR Code
-    </button>
-
-    {showScanner && (
-      <div
-        style={{
-          marginTop: '20px',
-          padding: '15px',
-          backgroundColor: 'white',
-          color: 'black',
-          borderRadius: '15px',
-          border: '3px solid black'
-        }}
-      >
-        <h2>
-          📷 Scan Physical Card
-        </h2>
-
-        <p>
-          Point your camera at the QR code on the card.
-        </p>
-
-        <div id="qr-reader"></div>
-
-        <button
-          onClick={() => setShowScanner(false)}
-        >
-          ❌ Close Scanner
-        </button>
-      </div>
-    )}
-  </>
-) : (
-  <p>
-    🏨 This is a hotel space.
-  </p>
-)}
-
 <hr />
           
       </div>
